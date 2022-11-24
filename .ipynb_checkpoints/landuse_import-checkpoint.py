fe34cc44fa74c7e2a34a2dff34091949f4ef4560
eyip <catmasteryip@users.noreply.github.com>
2022-11-24 18:17:49 +0800

    tracking reset 2

--- .ipynb_checkpoints/landuse_import-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import geopandas as gpd
-import shapely
-import matplotlib.pyplot as plt
-import pandas as pd
-import plotly.express as px
-
-district_dcca = pd.read_excel("../raw_data/SDI_2006_spatial map.xlsx",
-                             engine = "openpyxl")
